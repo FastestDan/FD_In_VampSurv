e4eeda8b466c722f447cf897547d3a9713afabe4
FastestDan <75726665+FastestDan@users.noreply.github.com>
2025-04-17 13:38:36 +1000

    Added Spear_Test weapon
    Right now it's hardcoded. Gotta find a way to un-hardcode it. Hopefully I'll be albe to do it.

--- Entities/Characters/player.gd
+++ Entities/Characters/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
 
-var SPEED = 100.0
+var SPEED = 150.0
 var hp = 100
+# Тут находятся оружия (смысл этого хардкода, если VampSurv - рогалик - непонятно)
+var spear = preload("res://Entities/Weapons/spear.tscn")
+
+@onready var speartimer = get_node("%SpearTimer")
+@onready var spearattacktimer = get_node("Attacks/SpearTimer/SpearAttackTimer")
+
+var spear_bullets = 0
+var spear_magazine = 1
+var spear_speed = 1.5
+var spear_level = 1
+
+var enemy_near = []
 
 
 func _physics_process(delta: float) -> void:
@@ -28,6 +40,47 @@ func movement():
 	$AnimatedSprite2D.play()
 
 
+func attack():
+	if spear_level>0:
+		speartimer.wait_time = spear.speed
+		if speartimer.is_stopped():
+			speartimer.start()
+
 func _on_hurt_box_hurt(damage: Variant) -> void:
 	hp -= damage
 	print(hp)
+
+
+func _on_spear_timer_timeout() -> void:
+	spear_bullets += spear_magazine
+	spearattacktimer.start()
+
+
+func _on_spear_attack_timer_timeout() -> void:
+	if spear_bullets > 0:
+		var spear_attack = spear.instantiate()
+		spear_attack.position = position
+		spear_attack.target = get_random_target()
+		spear_attack.level = spear_level
+		add_child(spear_attack)
+		spear_bullets -= 1
+		if spear_bullets > 0:
+			spearattacktimer.start()
+		else:
+			spearattacktimer.stop()
+		
+func get_random_target():
+	if enemy_near.size() > 0:
+		return enemy_near.pick_random().global_position
+	else:
+		return self.velocitya
+
+
+func _on_teki_detector_body_entered(body: Node2D) -> void:
+	if not enemy_near.has(body):
+		enemy_near.append(body)
+
+
+func _on_teki_detector_body_exited(body: Node2D) -> void:
+	if enemy_near.has(body):
+		enemy_near.erase(body)
---
+++ Entities/Weapons/spear.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+
+var level = 1
+var hp = 1
+var speed = 150
+var damage = 5
+var knock = 100
+var size = 1.0
+
+var target = Vector2.ZERO
+var angle = Vector2.ZERO
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _ready():
+	angle = global_position.direction_to(target)
+	rotation = angle.angle() + deg_to_rad(135)
+	match level:
+		1:
+			hp = 1
+			speed = 150
+			damage = 5
+			knock = 100
+			size = 1.0
+	
+func _physics_process(delta: float) -> void:
+	position += angle*speed*delta
+	
+	
+func enemy_hit(charge = 1):
+	hp -= charge
+	if hp <= 0:
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
