6dca530dc62f405dc37e7be570dd6613194f199b
FastestDan <75726665+FastestDan@users.noreply.github.com>
2025-06-24 03:54:31 +1000

    Added "Hurricane" kind of weapon
    Detects where you're looking and shoots there.

--- Entities/Characters/player.gd
+++ Entities/Characters/player.gd
@@ -5,28 +5,37 @@ var SPEED = 150.0
 var hp = 100
 # Тут находятся оружия (смысл этого хардкода, если VampSurv - рогалик - непонятно)
 var spear = preload("res://Entities/Weapons/spear.tscn")
+var shippu = preload("res://Entities/Weapons/shippu.tscn")
 
-@onready var speartimer = get_node("%SpearTimer")
-@onready var spearattacktimer = get_node("Attacks/SpearTimer/SpearAttackTimer")
+@onready var speartimer: Timer = get_node("%SpearTimer")
+@onready var spearattacktimer: Timer = speartimer.get_node("%SpearAttackTimer")
+@onready var shipputimer: Timer = get_node("%ShippuTimer")
+@onready var shippuattacktimer: Timer = shipputimer.get_node("%ShippuAttackTimer")
 
 var spear_bullets = 0
 var spear_magazine = 1
 var spear_speed = 1.5
-var spear_level = 1
+var spear_level = 0
 
-var prev_direction = Vector2.ZERO
-var curr_direction = Vector2.RIGHT
+var shippu_bullets = 0
+var shippu_magazine = 4
+var shippu_speed = 3
+var shippu_level = 1
+
+var prev_direction = Vector2.RIGHT
 
 var enemy_near = []
 
+func _ready() -> void:
+	attack()
 
 func _physics_process(delta: float) -> void:
 	movement()
 	
 func movement():
-	prev_direction = curr_direction
 	var vec = Input.get_vector("go_left", "go_right", "go_up", "go_down")
-	curr_direction = vec
+	if vec != Vector2.ZERO:
+		prev_direction = vec.round()
 	velocity = vec.normalized() * SPEED
 	#if velocity.length() > 0:
 		#$AnimatedSprite2D.play()
@@ -46,14 +55,19 @@ func movement():
 
 
 func attack():
-	if spear_level>0:
-		speartimer.wait_time = spear.speed
+	if spear_level > 0:
+		speartimer.wait_time = spear_speed
 		if speartimer.is_stopped():
 			speartimer.start()
+			
+	if shippu_level > 0:
+		shipputimer.wait_time = shippu_speed
+		if shipputimer.is_stopped():
+			shipputimer.start()
 
 func _on_hurt_box_hurt(damage, _angle, _knockback) -> void:
 	hp -= damage
-	print(hp)
+	#print(hp)
 
 
 func _on_spear_timer_timeout() -> void:
@@ -73,12 +87,32 @@ func _on_spear_attack_timer_timeout() -> void:
 			spearattacktimer.start()
 		else:
 			spearattacktimer.stop()
+			
+			
+func _on_shippu_timer_timeout() -> void:
+	shippu_bullets += shippu_magazine
+	shippuattacktimer.start()
+
+
+func _on_shippu_attack_timer_timeout() -> void:
+	if shippu_bullets > 0:
+		var shippu_attack = shippu.instantiate()
+		shippu_attack.position = position
+		shippu_attack.last_move = prev_direction
+		shippu_attack.level = shippu_level
+		add_child(shippu_attack)
+		shippu_bullets -= 1
+		if shippu_bullets > 0:
+			shippuattacktimer.start()
+		else:
+			shippuattacktimer.stop()
+
 		
 func get_random_target():
 	if enemy_near.size() > 0:
 		return enemy_near.pick_random().global_position
 	else:
-		return curr_direction
+		return prev_direction
 
 
 func _on_teki_detector_body_entered(body: Node2D) -> void:
---
+++ Entities/Weapons/shippu.gd
@@ -0,0 +1,80 @@
+extends Area2D
+
+
+var level = 1
+var hp = 9999
+var speed = 150
+var damage = 5
+var knockback = 100
+var size = 1.0
+
+var last_move = Vector2.ZERO
+var angle = Vector2.ZERO
+var angless = Vector2.ZERO
+var anglemore = Vector2.ZERO
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+signal remove_from_array(object)
+
+func _ready():
+	match level:
+		1:
+			hp = 9999
+			speed = 150
+			damage = 5
+			knockback = 100
+			size = 1.0
+			
+	var move_to_less = Vector2.ZERO
+	var move_to_more = Vector2.ZERO
+	match last_move:
+		Vector2.UP, Vector2.DOWN:
+			move_to_less = global_position + Vector2(randf_range(-1, -0.25), last_move.y) * 500
+			move_to_more = global_position + Vector2(randf_range(0.25, 1), last_move.y) * 500
+		Vector2.RIGHT, Vector2.LEFT:
+			move_to_less = global_position + Vector2(last_move.x, randf_range(-1, -0.25)) * 500
+			move_to_more = global_position + Vector2(last_move.x, randf_range(0.25, 1)) * 500
+		Vector2(1, 1), Vector2(1, -1), Vector2(-1, 1), Vector2(-1, -1):
+			move_to_less = global_position + Vector2(last_move.x, last_move.y * randf_range(0, 0.75)) * 500
+			move_to_more = global_position + Vector2(last_move.x * randf_range(0, 0.75), last_move.y) * 500
+	
+	angless = global_position.direction_to(move_to_less)
+	anglemore = global_position.direction_to(move_to_more)
+	
+	
+	var tween_start = create_tween().set_parallel(true)
+	tween_start.tween_property(self, "scale", Vector2(0.25, 0.25) * size, 3).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	var speed_final = speed
+	speed = speed / 5.0
+	tween_start.tween_property(self, "speed", speed_final, 6).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween_start.play()
+	
+	
+	var tween = create_tween()
+	var set_angle = randi_range(0, 1)
+	if set_angle == 1:
+		angle = angless
+		tween.tween_property(self, "angle", anglemore, 2)
+		tween.tween_property(self, "angle", angless, 2)
+	else:
+		angle = anglemore
+		tween.tween_property(self, "angle", angless, 2)
+		tween.tween_property(self, "angle", anglemore, 2)
+	tween.set_loops(3)
+	tween.play()
+	
+func _physics_process(delta: float) -> void:
+	position += angle*speed*delta
+	
+	
+func enemy_hit(charge = 1):
+	hp -= charge
+	if hp <= 0:
+		emit_signal("remove_from_array", self)
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	emit_signal("remove_from_array", self)
+	queue_free()
